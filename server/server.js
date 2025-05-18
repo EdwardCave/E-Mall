1bb437790bbf95b6030e7a8a2297cbd1612d6355
福福 <evisu@fufudeMacBook-Pro.local>
2025-05-18 23:12:28 +0800

    orders

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js'
+// import orderRouter from './routes/orderRoute.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+// app.use('/api/order',orderRouter)
 
 
 app.get('/', (req, res) => {
