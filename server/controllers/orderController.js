9bacb6e6586ea13ebcfcced2b6edd73d47ccece2
EdwardCave <EvisuChow@gmail.com>
2025-05-19 21:19:57 +0800

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,11 @@
 import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModel.js"
 
+import Stripe from "stripe"
+
+
+// const stripe = new Stripe(stripe_key)
+
 const currency = "pkr"
 const deliveryCharges = 10
 // controller function for placing order using cod method
@@ -31,16 +36,83 @@ const placeOrder = async(req,res) => {
 
 // controller function for placing order using stripe method
 
-const placeOrderStripe = async(req,res) => {
-    try{
-        const {userId,items,amount,address,paymentMethod} = req.body
-        const order = new orderModel({userId,items,amount,address,paymentMethod})
-        await order.save()
-        res.json({success:true, message:"Order Placed Successfully"})
-    }catch(error){
-        res.json({success:false, message:error.message})
-    }
-}
+// const placeOrderStripe = async(req,res) => {
+//     try{
+//         const {userId,items,amount,address} = req.body
+
+//         const {origin} = req.headers
+
+//         const orderData = {
+//             userId,
+//             items,
+//             amount,
+//             address,
+//             paymentMethod:"Stripe",
+//             payment:false,
+//             date:Date.now()
+//         }
+
+//         const order = new orderModel(orderData)
+
+//         await order.save()
+
+//         const line_items = items.map(item => ({
+          
+//                 price_data: {
+//                     currency: currency,
+//                     product_data: {
+//                         name: item.name,
+                        
+//                     },
+//                     unit_amount: item.price * 100 * 277,
+//                 },
+//                 quantity: item.quantity,
+          
+//         }))
+//         line_items.push({
+//             price_data: {
+//                 currency: currency,
+//                 product_data: {
+//                     name: "Delivery Charges",
+                    
+//                 },
+//                 unit_amount: deliveryCharges * 100 * 277,
+//             },
+//             quantity: 1,
+//         })
+
+//         const session = await stripe.checkout.sessions.create({
+//             payment_method_types: ['card'],
+//             line_items,
+//             mode: 'payment',
+//             success_url: `${origin}/verify?success=true&orderId=${order._id}`,
+//             cancel_url: `${origin}/verify?success=false&orderId=${order._id}`,
+//            
+//         })
+//         res.json({success:true, session_url: session.url})
+//     }catch(error){
+//         res.json({success:false, message:error.message})
+//     }
+// }
+
+
+// controller function for verifying stripe(this is a temporary method for test)
+
+// const verifyStripe = async(req,res) => {
+//     const {orderId,success,userId} = req.body
+//     try{
+//         if(success === 'true'){
+//             await orderModel.findByIdAndUpdate(orderId,{payment:true})
+//             await userModel.findByIdAndUpdate(userId,{cartData:{}})
+//             res.json({success:true})
+//         }else{
+//             await orderModel.findByIdAndDelete(orderId)
+//             res.json({success:false})
+//         }
+//     }catch(error){
+//         res.json({success:false, message:error.message})
+//     }
+// }
 
 // controller function for getting all  orders data for admin panel 
 const allOrders = async(req,res) => {
@@ -80,4 +152,4 @@ const updateStatus = async(req,res) => {
     
 }
 
-export {placeOrder,placeOrderStripe,userOrders,allOrders,updateStatus}
+export {placeOrder,placeOrderStripe,userOrders,allOrders,updateStatus,verifyStripe}
